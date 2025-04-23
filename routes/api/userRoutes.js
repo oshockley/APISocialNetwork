32294ec47c570a0337f2bd70a882557141363434
O'Neal Shockley <shockleyoneal@gmail.com>
2025-04-23 16:00:52 -0700

    made changes to multiple files

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users GET all and POST
+router.route('/').get(getUser).post(createUser);
+
+// /api/users/:userID GET (one user) PUT & DELETE by user's ID
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId POST & DELETE a friend with ID
+router.route('/userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
